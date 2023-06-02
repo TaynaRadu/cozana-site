8cc931531c6f387157719bd29d05e6fcda7767f1
TaynaRadu <tay.angelo1994@gmail.com>
2023-06-02 15:50:43 +0100

    feat: update Gallery componente

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,25 @@ import { Gallery } from "./components/Gallery/index";
 import { Footer } from "./components/Footer/index";
 import { Header } from "./components/Header/components/Header/index";
 import cozonac1 from "./assets/images/cozonac1.png";
+import cake1 from "./assets/images/cake1.jpg";
+import cake2 from "./assets/images/cake2.jpg";
+import cake3 from "./assets/images/cake3.jpeg";
+import cake4 from "./assets/images/cake4.jpeg";
+import cake5 from "./assets/images/cake5.jpeg";
+import cake6 from "./assets/images/cake6.jpeg";
+import cake7 from "./assets/images/cake7.jpeg";
+import cake8 from "./assets/images/cake8.jpeg";
+
+const IMAGES = [
+  { img: cake1, alt: "cake1" },
+  { img: cake2, alt: "cake2" },
+  { img: cake3, alt: "cake3" },
+  { img: cake4, alt: "cake4" },
+  { img: cake5, alt: "cake5" },
+  { img: cake6, alt: "cake6" },
+  { img: cake7, alt: "cake7" },
+  { img: cake8, alt: "cake8" },
+];
 
 function App() {
   return (
@@ -11,7 +30,7 @@ function App() {
       <Header />
       <Banner img={cozonac1} text="Făcut cu dragoste" />
       <Description />
-      <Gallery />
+      <Gallery images={IMAGES} />
       <Footer />
     </>
   );
---
+++ src/components/Gallery/types.ts
@@ -0,0 +1,8 @@
+export type GalleryImage = {
+  img: string;
+  alt: string;
+};
+
+export type GalleryProps = {
+  images: GalleryImage[];
+};
